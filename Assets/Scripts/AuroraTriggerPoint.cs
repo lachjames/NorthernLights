ea9ac69309a1598ae9ca8464f3115952a8fc609f
Lachlan O'Neill <me@lachlanoneill.io>
2021-01-29 13:16:46 +1100

    Fixed structid problem for new trigger points

--- Assets/Scripts/AuroraTriggerPoint.cs
+++ Assets/Scripts/AuroraTriggerPoint.cs
@@ -15,6 +15,7 @@ public class AuroraTriggerPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        geometry.structid = 3;
         geometry.PointX = transform.localPosition.x;
         geometry.PointY = transform.localPosition.z;
         geometry.PointZ = transform.localPosition.y;
